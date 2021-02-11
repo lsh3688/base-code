cc64d88ac35cf94af6492005e60f5ea49e793e25
lsh <18103523688@189.cn>
2021-02-11 17:40:55 +0800

    第一次提交

---
+++ day01-code/src/com/icss/oa/Student.java
@@ -0,0 +1,18 @@
+package com.icss.oa;
+
+
+import java.util.Random;
+
+public class Student {
+    public static void main(String[] args) {
+        int i=10;
+        int j=20;
+
+        int sum = i+j;
+
+        System.out.println("sum:"+sum);
+
+
+    }
+
+}
